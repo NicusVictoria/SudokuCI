add0aa8183cc22417a284e2ddcaca773604d0365
bnunen <bnunen@hotmail.com>
2015-06-19 23:36:48 +0200

    It's working only it is not solving :(

--- sudoku9x9.py
+++ sudoku9x9.py
@@ -12,21 +12,23 @@
 ##   values is a dict of possible values, e.g. {'A1':'12349', 'A2':'8', ...}
 import random
 
+
 def cross(A, B):
     "Cross product of elements in A and elements in B."
-    return [a+b for a in A for b in B]
+    return [a + b for a in A for b in B]
 
-digits   = '123456789'
-rows     = 'ABCDEFGHI'
-cols     = digits
-squares  = cross(rows, cols)
-blocks   = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+digits = '123456789'
+rows = 'ABCDEFGHI'
+cols = digits
+squares = cross(rows, cols)
+blocks = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
 unitlist = ([cross(rows, c) for c in cols] +
             [cross(r, cols) for r in rows] +
             blocks)
 units = dict((s, [u for u in unitlist if s in u])
              for s in squares)
-peers = dict((s, set(sum(units[s],[]))-set([s]))
+peers = dict((s, set(sum(units[s], [])) - set([s]))
              for s in squares)
 
 ################ Unit Tests ################
@@ -43,7 +45,8 @@ def test():
     assert peers['C2'] == set(['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2',
                                'C1', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
                                'A1', 'A3', 'B1', 'B3'])
-    print( 'All tests pass.')
+    print('All tests pass.')
+
 
 ################ Parse a Grid ################
 
@@ -52,16 +55,18 @@ def parse_grid(grid):
     return False if a contradiction is detected."""
     ## To start, every square can be any digit; then assign values from the grid.
     values = dict((s, digits) for s in squares)
-    for s,d in grid_values(grid).items():
+    for s, d in grid_values(grid).items():
         if d in digits and not assign(values, s, d):
-            return False ## (Fail if we can't assign d to square s.)
+            return False  ## (Fail if we can't assign d to square s.)
     return values
+
 
 def grid_values(grid):
     "Convert grid into a dict of {square: char} with '0' or '.' for empties."
     chars = [c for c in grid if c in digits or c in '0.']
     assert len(chars) == 81
     return dict(zip(squares, chars))
+
 
 ################ Constraint Propagation ################
 
@@ -74,15 +79,16 @@ def assign(values, s, d):
     else:
         return False
 
+
 def eliminate(values, s, d):
     """Eliminate d from values[s]; propagate when values or places <= 2.
     Return values, except return False if a contradiction is detected."""
     if d not in values[s]:
-        return values ## Already eliminated
-    values[s] = values[s].replace(d,'')
+        return values  ## Already eliminated
+    values[s] = values[s].replace(d, '')
     ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
     if len(values[s]) == 0:
-        return False ## Contradiction: removed last value
+        return False  ## Contradiction: removed last value
     elif len(values[s]) == 1:
         d2 = values[s]
         if not all(eliminate(values, s2, d2) for s2 in peers[s]):
@@ -91,121 +97,131 @@ def eliminate(values, s, d):
     for u in units[s]:
         dplaces = [s for s in u if d in values[s]]
         if len(dplaces) == 0:
-            return False ## Contradiction: no place for this value
+            return False  ## Contradiction: no place for this value
         elif len(dplaces) == 1:
             # d can only be in one place in unit; assign it there
             if not assign(values, dplaces[0], d):
                 return False
     return values
 
+
 ################ Display as 2-D grid ################
 
 def display(values):
     "Display these values as a 2-D grid."
-    width = 1+max(len(values[s]) for s in squares)
-    line = '+'.join(['-'*(width*3)]*3)
+    width = 1 + max(len(values[s]) for s in squares)
+    line = '+'.join(['-' * (width * 3)] * 3)
     for r in rows:
         print(''.join(values[r + c].center(width) + ('|' if c in '36' else '')
                       for c in cols))
         if r in 'CF': print(line)
-    print
 
 ################ Search ################
 
-def solve(grid): return search(parse_grid(grid))
+def solve(grid): return localSeurchStart(parse_grid(grid)) #was search
+
 
 def search(values):
     "Using depth-first search and propagation, try all possible values."
     if values is False:
-        return False ## Failed earlier
+        return False  ## Failed earlier
     if all(len(values[s]) == 1 for s in squares):
-        return values ## Solved!
+        return values  ## Solved!
     ## Chose the unfilled square s with the fewest possibilities
-    n,s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
+    n, s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
     return some(search(assign(values.copy(), s, d))
                 for d in values[s])
 
+
 ####### Local Search #######
 def localSeurch(values, board, wrongValues, score, totalScore):
-    newBoard, check = seekSwitchBlocks(values, board, wrongValues)
-    print(check)
-    rowsToCheck = (vield[1] for vield in check)
-    colsToCheck = (vield[2] for vield in check)
-    newScore = evaluation(values, score, rowsToCheck, colsToCheck)
-    newTotalScore = scoreTotal(newScore)
-    if newTotalScore == 0:
-        return values ## Solved!
-    if newTotalScore > totalScore:
-        wrongValues = calcWrongValues (values, newBoard, wrongValues, rowsToCheck, colsToCheck)
-        return localSeurch(values, newBoard, wrongValues, newScore, newTotalScore)
-
+    for _ in range(0,200): #to prevent never ending loops
+        newBoard, check = seekSwitchBlocks(values, board, wrongValues)
+        if check[1]=='':    #if there was no second swich item
+            if all(len(wrongValues[s]) == 0 for s in squares):
+                return board  ## Solved!
+            else:
+                return board ## error no answer
+        rowsToCheck = ''.join(vield[0] for vield in check)
+        colsToCheck = ''.join(vield[1] for vield in check)
+        newScore = evaluation(values, score, rowsToCheck, colsToCheck)
+        newTotalScore = scoreTotal(newScore)
+        if newTotalScore < totalScore:
+            wrongValues = calcWrongValues(values, newBoard, wrongValues, rowsToCheck, colsToCheck)
+            return localSeurch(values, newBoard, wrongValues, newScore, newTotalScore)
+    return board ##out of time range
 
 def localSeurchStart(values):
     board = constructBoard(values)
-    score = dict((s, 9) for s in rows + cols)#make a dictionary for the score
+    score = dict((s, 9) for s in rows + cols)  # make a dictionary for the score
     score = evaluation(board, score, rows, cols)
-    wrongValues = dict((s, '') for s in squares)#make a dictionary for the wrongValues
+    wrongValues = dict((s, '') for s in squares)  # make a dictionary for the wrongValues
     wrongValues = calcWrongValues(values, board, wrongValues, rows, cols)
-    localSeurch(values, board, wrongValues, score, scoreTotal(score))
+    return localSeurch(values, board, wrongValues, score, scoreTotal(score))
 
 
-def calcWrongValues    (values, board, oldWrongValues, rowsToCheck, colsToCheck):
+def calcWrongValues(values, board, oldWrongValues, rowsToCheck, colsToCheck):
     wrongValues = oldWrongValues
-    for r in rowsToCheck: #only check the changed rows and cols
+    for r in rowsToCheck:  # only check the changed rows and cols
         for c in cols:
-            s = r+c
-            if len(values[s]) > 1: #if there are no multiple options for a position in cant be wrong
-                if board[s] not in values[s]: #if the value is not in is's possible values in never van be right
+            s = r + c
+            wrongValues[s] = ''
+            if len(values[s]) > 1:  # if there are no multiple options for a position in cant be wrong
+                if board[s] not in values[s]:  # if the value is not in is's possible values in never van be right
                     wrongValues[s] = board[s]
-                elif board[s] not in (values[peer] for peer in peers[s]):    #if this value also is in it's peers
+                elif board[s] in (board[peer] for peer in peers[s]):  # if this value also is in it's peers
                     wrongValues[s] = board[s]
-                else:
-                    wrongValues[s] = ''
     for c in colsToCheck:
         for r in rows:
-            s = r+c
+            s = r + c
+            if r not in rowsToCheck:
+                wrongValues[s] = ''
             if len(values[s]) > 1:
                 if board[s] not in values[s]:
                     wrongValues[s] = board[s]
-                elif board[s] not in (values[peer] for peer in peers[s]):
+                elif board[s] in (board[peer] for peer in peers[s]):
                     wrongValues[s] = board[s]
-                else:
-                    wrongValues[s] = ''
     return wrongValues
 
+
 def constructBoard(values):
+    board = values.copy()
     for block in blocks:
         possible = digits
         toDo = []
-        for i in range(0, len(block)-1): #always place the first of the possible values
+        list = (range(0, len(block)))
+        for i in range(0, len(block)):  # always place the first of the possible values
             vield = block[i]
-            if len(values[vield])>0 : #if there is nothing possible in should be given the rest value
-                value = values[vield][0]
-                for j in range(i, len(block)-1):
-                    values[block[j]] = values[block[j]].replace(value,'')
-                values[vield] = value
-                possible = possible.replace(value,'')
+            if len(board[vield]) > 0:  # if there is nothing possible in should be given the rest value
+                value = board[vield][0]
+                for j in range(i, len(block)):
+                    board[block[j]] = board[block[j]].replace(value, '')
+                board[vield] = value
+                possible = possible.replace(value, '')
             else:
-                toDo.add(vield)
-        for i in range(0, len(toDo)-1):
-            values[toDo[i]] = possible[i]
-    return values
+                toDo = toDo + [vield]
+        for x in range(0, len(toDo)):
+            board[toDo[x]] = possible[x]
+    return board
+
 
 def evaluation(values, score, rowsToCheck, colsToCheck):
-    for r in rowsToCheck: #check witch number is not there in each row and column and count the length
+    for r in rowsToCheck:  # check witch number is not there in each row and column and count the length
         possible = digits
         for c in cols:
-            possible = possible.replace(values[r+c],'')
+            possible = possible.replace(values[r + c], '')
         score[r] = len(possible)
     for c in colsToCheck:
         possible = digits
         for r in rows:
-            possible = possible.replace(values[r+c],'')
+            possible = possible.replace(values[r + c], '')
         score[c] = len(possible)
     return score
 
+
 def scoreTotal(score):
     return sum(score.values())
+
 
 def seekSwitchBlocks(values, board, wrongValues):
     swichVield = ''
@@ -214,13 +230,14 @@ def seekSwitchBlocks(values, board, wrongValues):
         random.shuffle(block)
         for vield in block:
             if wrongValues[vield] != '':
-                for swichVield in rndvields:
+                for swichVield in block:
                     if swichVield != vield and board[vield] in values[swichVield]:
-                    #can be better by checking if also board[swichVield] in values[vield] but then it can hafe no autput
+                        # can be better by checking if also board[swichVield] in values[vield] but then it can hafe no autput
                         board[vield] = board[swichVield]
                         board[swichVield] = wrongValues[vield]
                         break
     return board, [vield, swichVield]
+
 
 ################ Utilities ################
 
@@ -230,9 +247,11 @@ def some(seq):
         if e: return e
     return False
 
+
 def from_file(filename, sep='\n'):
     "Parse a file into a list of strings, separated by sep."
     return open(filename).read().strip().split(sep)
+
 
 def shuffled(seq):
     "Return a randomly shuffled copy of the input sequence."
@@ -244,20 +263,23 @@ def shuffled(seq):
 
 import time, random
 
+
 def solve_all(grids, name='', showif=0.0):
     """Attempt to solve a sequence of grids. Report results.
     When showif is a number of seconds, display puzzles that take longer.
     When showif is None, don't display any puzzles."""
+
     def time_solve(grid):
         start = time.clock()
         values = solve(grid)
-        t = time.clock()-start
+        t = time.clock() - start
         ## Display puzzles that take long enough
         if showif is not None and t > showif:
             display(grid_values(grid))
             if values: display(values)
             print('(%.2f seconds)\n' % t)
         return (t, solved(values))
+
     times, results = zip(*[time_solve(grid) for grid in grids])
     N = len(grids)
     if N > 1:
@@ -268,7 +290,9 @@ def solve_all(grids, name='', showif=0.0):
 def solved(values):
     "A puzzle is solved if each unit is a permutation of the digits 1 to 9."
     def unitsolved(unit): return set(values[s] for s in unit) == set(digits)
+
     return values is not False and all(unitsolved(unit) for unit in unitlist)
+
 
 def random_puzzle(N=17):
     """Make a random puzzle with N or more assignments. Restart on contradictions.
@@ -280,25 +304,25 @@ def random_puzzle(N=17):
             break
         ds = [values[s] for s in squares if len(values[s]) == 1]
         if len(ds) >= N and len(set(ds)) >= 8:
-            return ''.join(values[s] if len(values[s])==1 else '.' for s in squares)
-    return random_puzzle(N) ## Give up and make a new puzzle
+            return ''.join(values[s] if len(values[s]) == 1 else '.' for s in squares)
+    return random_puzzle(N)  ## Give up and make a new puzzle
 
-grid1  = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
-grid2  = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
-hard1  = '.....6....59.....82....8....45........3........6..3.54...325..6..................'
-    
+
+grid1 = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
+grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+hard1 = '.....6....59.....82....8....45........3........6..3.54...325..6..................'
+
 if __name__ == '__main__':
-    test()
-    solution = localSeurchStart(parse_grid(grid1))
-    display(solution)
-    #print(solved(solution))
-    #solve_all(from_file("easy50.txt", '========'), "easy", None)
-    #solve_all(from_file("top95.txt"), "hard", None)
-    #solve_all(from_file("hardest.txt"), "hardest", None)
-    #solve_all([random_puzzle() for _ in range(99)], "random", 100.0)
+    #test()
+    #solution = localSeurchStart(parse_grid(grid1))
+    #display(solution)
+    solve_all(from_file("easy50.txt", '========'), "easy", None)
+    solve_all(from_file("top95.txt"), "hard", None)
+    solve_all(from_file("hardest.txt"), "hardest", None)
+    solve_all([random_puzzle() for _ in range(99)], "random", 100.0)
 
-## References used:
-## http://www.scanraid.com/BasicStrategies.htm
-## http://www.sudokudragon.com/sudokustrategy.htm
-## http://www.krazydad.com/blog/2005/09/29/an-index-of-sudoku-strategies/
-## http://www2.warwick.ac.uk/fac/sci/moac/currentstudents/peter_cock/python/sudoku/
+    ## References used:
+    ## http://www.scanraid.com/BasicStrategies.htm
+    ## http://www.sudokudragon.com/sudokustrategy.htm
+    ## http://www.krazydad.com/blog/2005/09/29/an-index-of-sudoku-strategies/
+    ## http://www2.warwick.ac.uk/fac/sci/moac/currentstudents/peter_cock/python/sudoku/
